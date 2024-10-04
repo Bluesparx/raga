3deb74427282242295d4e668225c3965218b3dba
nazia <naziahassan2288@gmail.com>
2024-10-04 19:18:23 +0530

    remove node modules

--- node_modules/emailjs-com/es/methods/init/init.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Initiation
- * @param {string} userID - set the EmailJS user ID
- * @param {string} origin - set the EmailJS origin
- */
-export declare const init: (userID: string, origin?: string) => void;
--- node_modules/emailjs-com/es/utils/validateParams.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const validateParams: (userID?: string | undefined, serviceID?: string | undefined, templateID?: string | undefined) => boolean;
