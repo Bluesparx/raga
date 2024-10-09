1127163ff08be29b55dda8871232a8799d77794f
karan <karansawan5050@gmail.com>
2024-10-10 01:37:49 +0530

    add page not found page and optimized routing in app.jsx

--- frontend/src/utils/navbarSwitch.jsx
+++ frontend/src/utils/navbarSwitch.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { useAuth } from './authProvider';
-import {NavbarDemo} from "../components/NavbarDemo";
-import {Navbar2} from "../components/Navbar2";
+import { useAuth } from "./authProvider";
+import { NavbarDemo } from "../components/NavbarDemo";
+import { Navbar2 } from "../components/Navbar2";
 
 const NavbarSwitch = () => {
   const { token } = useAuth();
 
-  return token && token !== null ? <Navbar2 /> : <NavbarDemo />
+  return token && token !== null ? <Navbar2 /> : <NavbarDemo />;
 };
 
 export default NavbarSwitch;
