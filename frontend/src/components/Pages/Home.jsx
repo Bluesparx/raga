2b554a28c50070b285fc45ce05d4e46969ac4cbf
Nazia Hassan <116333715+Bluesparx@users.noreply.github.com>
2024-10-10 10:36:29 +0530

    Revert "ADDED GET STARTED BUTTON TO HOMESCREEN"

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,37 +1,14 @@
-import React from "react";
-import { WavyBackgroundDemo } from "../WavyBackgroundDemo";
-import { Navbar2 } from "../Navbar2";
-import { NavbarDemo } from "../NavbarDemo";
-import { useNavigate } from "react-router-dom";
-
+import React from 'react'
+import { WavyBackgroundDemo } from '../WavyBackgroundDemo'
+import { Navbar2 } from '../Navbar2'
+import { NavbarDemo } from '../NavbarDemo'
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleGetStarted = () => {
-    navigate("/register");
-  };
   return (
     <div>
       {/* <Navbar2/> */}
-      <WavyBackgroundDemo />
-
-      <div className="mt-8 w-full flex justify-center">
-        <button
-          className=" absolute px-6 py-3 bg-transparent border-2
-          top-[75%] 
-          w-[200px]
-          h-[60px]
-          border-pink-400 text-pink-500 rounded-full
-          hover:bg-pink-500
-          hover:text-black
-          hover:border-black"
-          onClick={handleGetStarted}
-        >
-          Get Started
-        </button>
-      </div>
+      <WavyBackgroundDemo/>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- frontend/src/components/WavyBackgroundDemo.jsx
+++ frontend/src/components/WavyBackgroundDemo.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-
 import { WavyBackground } from "./ui/wavy-background";
 
 export function WavyBackgroundDemo() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen">
-      <div>
-        <WavyBackground className="max-w-4xl mx-auto pb-40">
-          <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-            Zen Zone
-          </p>
-          <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-            `sit in meditation for 20 minutes a day. Unless you’re too busy,
-            then sit for an hour.`
-          </p>
-        </WavyBackground>
-      </div>
-    </div>
+    <WavyBackground className="max-w-4xl mx-auto pb-40" >
+      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        Zen Zone
+      </p>
+      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+       `sit in meditation for 20 minutes a day. Unless you’re too busy, then sit for an hour.`
+      </p>
+    </WavyBackground>
   );
 }
 
