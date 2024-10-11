5b6a009d7bbdfd880c596cb8c4e9335e4747ed6a
nazia <naziahassan2288@gmail.com>
2024-10-11 14:32:57 +0530

    Fix Firebase

--- frontend/src/components/Pages/GoogleAuthButton.jsx
+++ frontend/src/components/Pages/GoogleAuthButton.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from './Firebase.js';
 import GoogleButton from 'react-google-button'
+import { useAuth } from '../../utils/authProvider.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const GoogleAuthButton = () => {
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  
   const handleGoogleSignIn = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      console.log("User Info: ", user);
-      
+      // console.log("User Info: ", user);
+      const token = user.accessToken;
+      login(token);
+      navigate("/");
     } catch (error) {
       console.error("Error during sign-in:", error);
       
