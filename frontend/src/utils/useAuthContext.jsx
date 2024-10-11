4662bbd4534cdbae853522ebb3fabb4b16bf787b
Mohammed Abdur Rahmaan <112614695+abdurrahmaan11265@users.noreply.github.com>
2024-10-11 19:02:59 +0530

    Restrict access to login and Register routes for authenticated users

--- frontend/src/utils/useAuthContext.jsx
+++ frontend/src/utils/useAuthContext.jsx
@@ -1,9 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./authProvider";
+import LoginForm from "../components/Pages/Login.jsx";
+import SignupForm from "../components/Pages/Signup.jsx";
 
 const AuthenticatedRoute = ({ element }) => {
   const { token } = useAuth();
 
+  if (token && (element.type === LoginForm || element.type === SignupForm)) {
+    return <Navigate to="/" />;
+  }
+  if (!token && (element.type === LoginForm || element.type === SignupForm)) {
+    return element; // Render login form if no token
+  }
   if (token) {
     return element; // If token exists, render the requested component
   } else {
