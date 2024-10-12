8830cec713f9a6996450a2006cf7ec26fd86856c
nazia <naziahassan2288@gmail.com>
2024-10-12 14:34:45 +0530

    google login icon

--- frontend/src/components/Pages/GoogleAuthButton.jsx
+++ frontend/src/components/Pages/GoogleAuthButton.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from './Firebase.js';
-import GoogleButton from 'react-google-button'
 import { useAuth } from '../../utils/authProvider.jsx';
 import { useNavigate } from 'react-router-dom';
-import Google from '../../assets/google.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
-const GoogleAuthButton = ({text}) => {
-
+const GoogleAuthButton = ({ text }) => {
   const { login } = useAuth();
   const navigate = useNavigate();
   
@@ -15,23 +14,22 @@ const GoogleAuthButton = ({text}) => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      // console.log("User Info: ", user);
       const token = user.accessToken;
       login(token);
       navigate("/");
     } catch (error) {
       console.error("Error during sign-in:", error);
-      
     }
   };
   
-
   return (
-    <button className="flex gap-2 items-center justify-center rounded-lg border-violet-400 border w-full py-3">
-      <img src={Google} className="h-4 w-4" alt="" />
+    <button 
+      className="flex gap-2 items-center justify-center rounded-lg border-violet-400 border w-full py-3"
+      onClick={handleGoogleSignIn}
+    >
+      <FontAwesomeIcon icon={faGoogle} className="h-4 w-4 text-white" />
       <p className="text-sm text-white">{text}</p>
     </button>
-
   );
 };
 
